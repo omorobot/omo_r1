c87bbd3c807531af955153b7b078da9aa0945f7d
Bishop Pearson <bishopearson@gmail.com>
2020-02-02 22:01:03 +0900

    modified for package redefinition

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# omo
+# OMO R1 unofficial ROS package
 Unofficial ros package for OMO R1 robot. The original source has been released https://github.com/omorobot/omoros  
--- omo_r1_teleop/setup.py
+++ omo_r1_teleop/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['omo_teleop'],
+    packages=['omo_r1_teleop'],
     package_dir={'': 'src'}
 )
 
