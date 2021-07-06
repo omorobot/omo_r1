99e0b50a2c42fab4d58d7b6664ada6d383698f5c
PinkWink <pinkwink.korea@gmail.com>
2021-07-07 07:44:05 +0900

    lift

--- omo_r1_bringup/bash_setting/init_sh.sh
+++ omo_r1_bringup/bash_setting/init_sh.sh
@@ -1,11 +1,13 @@
 echo "initialize robot"
 
-echo "alias connect_1 = 'ssh r1@192.168.<ip1.ip1>'" >> ~/.bashrc
-echo "alias connect_2 = 'ssh r1@192.168.<ip2.ip2>'" >> ~/.bashrc
+echo "alias connect_1='ssh r1@192.168.<ip1.ip1>'" >> ~/.bashrc
+echo "alias connect_2='ssh r1@192.168.<ip2.ip2>'" >> ~/.bashrc
 
-echo "alias ready_robot = 'roslaunch omo_r1_bringup ready_robot.launch set_port:=/dev/ttyUSB0 set_lift_port:=/dev/ttyUSB1'" >> ~/.bashrc
-echo "alias go_lift_high = 'rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 800\"'" >> ~/.bashrc
-echo "alias go_lift_middel = 'rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 600\"'" >> ~/.bashrc
-echo "alias go_lift_bottom = 'rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 400\"'" >> ~/.bashrc
+echo "alias ready_robot='roslaunch omo_r1_bringup ready_robot.launch set_port:=/dev/ttyUSB0 set_lift_port:=/dev/ttyUSB1'" >> ~/.bashrc
+echo "alias go_lift_high='rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 800\"'" >> ~/.bashrc
+echo "alias go_lift_middel='rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 600\"'" >> ~/.bashrc
+echo "alias go_lift_bottom='rostopic pub -r 1 /lift_pose std_msgs/Int32 \"data: 400\"'" >> ~/.bashrc
 
-echo "alias ready_key = 'roslaunch omo_r1_teleop omo_r1_teleop_key.launch'" >> ~/.bashrc
+echo "alias ready_key='roslaunch omo_r1_teleop omo_r1_teleop_key.launch'" >> ~/.bashrc
+
+source ~/.bashrc
